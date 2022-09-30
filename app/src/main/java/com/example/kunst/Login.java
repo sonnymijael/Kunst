7fee0fa977e29bdebd8288730ecc2cb7ee4f170a
Sonny Ahumada <sonnymijael@gmail.com>
2022-09-30 12:47:26 -0500

    login activity

--- app/src/main/java/com/example/kunst/Login.java
+++ app/src/main/java/com/example/kunst/Login.java
@@ -1,30 +1,54 @@
 package com.example.kunst;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class Login extends AppCompatActivity {
+import com.squareup.picasso.Picasso;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    Button Ingresar;
+    ImageView Logo;
+    String logo_url = "https://api.sonnymijael.com/v1/kusnt/resources/logo.png";
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    /// Preguntar al usuario si tiene cuenta
-    /// Register => form
-    /// Datos a solicitar
-    /// Front => save local storage === servidor | se actualiza cada vez que se inicia session
-    /// Encriptacion de contraseña
+        // Set header color
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.actionbar));
 
-//        if(register_customer()){
-//            if(is_doctor()){
-//
-//            }
-//        }
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#557C83"));
+        actionBar.setBackgroundDrawable(colorDrawable);
+
+        Logo = (ImageView) findViewById(R.id.logo);
+        Picasso.get().load(logo_url).into(Logo);
+
+        Ingresar = (Button) findViewById(R.id.ingresar);
+        Ingresar.setOnClickListener(this);
+
+        intent = new Intent();
+
+        setTitle("Sign In");
     }
 
     public boolean register_customer(){
         return false;
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
